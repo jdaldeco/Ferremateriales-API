f379153b3a86ee494a052dd1ee0ac4ff249c2b1b
jdaldeco <jdaldeco@gmail.com>
2020-12-02 20:24:47 -0700

    Cambios en ventas
    Retro de ramses

--- server/models/Venta.js
+++ server/models/Venta.js
@@ -1,28 +1,26 @@
 const mongoose = require('mongoose');
-var Producto = require('./Producto');
-var Cliente = require('./Cliente');
 const Schema = mongoose.Schema;
 
 var ventaSchema = new Schema({
     folio: {
         type: String,
-        default: "",
+        default: ""
     },
-    clientes: {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref:'Cliente'
-    },
-    productos: {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref:'Producto'
+    productos: [{
+        type: String,
+        default: ""
+    }],
+    cilente: {
+        type: String, 
+        default: ""
     },
     importe: {
         type: Number,
-        default: "0.0",
+        default: "0.0"
     },
     fecha: {
         type: Date,
-        default: "",
+        default: ""
     },
 });
 
